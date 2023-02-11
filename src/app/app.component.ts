67f86c309f76a80ffb1c298c258351f406702763
surya <itzmesurya@gmail.com>
2023-02-12 02:08:02 +0530

    Added filter pipe and api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bootstrap-autocomplete';
+  filterText: string = '';
+  items: string[] = ['Another Action',
+    'Something else is here']
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://reqres.in/api/users')
+      .subscribe((resp: any) => {
+        this.items = resp['data'].map((i: { first_name: any; last_name: any; }) => i.first_name + i.last_name);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NgbModule,
     NgbDropdownModule
   ],
